73ac45b7c0f5c64ef05740bdd0f36079d5a8526d
Alejandro Hernández Andrade <quijoteaha@gmail.com>
2024-03-14 14:34:58 -0600

    JS 2-objetos

--- 3-javascript/2-objetos/ejercicio.js
+++ 3-javascript/2-objetos/ejercicio.js
@@ -21,9 +21,11 @@
 const juegos = {
     aguila: 0,
     sol: 1,
-    tirarMoneda: undefined
+    tirarMoneda(){
+        Math.floor(Math.random() * 2) == 0 ? console.log('"Cae águila 🦅"') : console.log('"Cae sol 🌞"');
+    }
 }
 
 for (let tiros = 0; tiros < 10; tiros++) {
-    // juegos.tirarMoneda()
+    juegos.tirarMoneda()
 }
